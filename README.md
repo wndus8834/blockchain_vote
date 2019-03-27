cd60ba4678d74a4bc48c0d7303c5e30f956b2f6a
wndus8834 <wndus8834@naver.com>
2019-03-28 07:15:16 +0900

    Update

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# blockchain_vote
+## blockchain_vote
+
+웹과 mist 프로그램을 연결시키기 위해서는 컨트랙트를 생성하고 그 주소가 필요했습니다. 그래서 컨트랙트를 알아보기 위해 투표 예제를 실행해보았습니다.
+
+우선 mist 프로그램 내에서 컨트랙트를 생성하고 생성된 컨트랙트의 인터페이스와 주소를 복사합니다.
+
+![image](https://user-images.githubusercontent.com/38427658/55115652-61088200-5128-11e9-9da3-7847467349c0.png)
+
+
+그 후 html파일 소스 내에 있는 문장을 수정하면 웹과 mist 프로그램이 연결되고 구동이 가능해집니다.
+```html
+var vc = web3.eth.contract
+```
+![image](https://user-images.githubusercontent.com/38427658/55115716-87c6b880-5128-11e9-82b5-6e7c29357336.png)
---
+++ vote.sol
@@ -0,0 +1,72 @@
+pragma solidity ^0.4.11;
+
+contract voteContract {
+
+    mapping (address => bool) voters; // 하나의 계정 당 한 번의 투표만 가능
+    mapping (string => uint) candidates; // 후보자의 득표수를 저장합니다.
+    mapping (uint8 => string) candidateList; // 후보자의 리스트입니다.
+
+    uint8 numberOfCandidates; // 총 후보자의 수입니다.
+    address contractOwner;
+
+    function voteContract() {
+        contractOwner = msg.sender;
+    }
+
+    // 후보자를 추가하는 함수입니다.
+    function addCandidate(string cand) {
+        bool add = true;
+        for (uint8 i = 0; i < numberOfCandidates; i++) {
+        
+            // 문자열 비교는 해쉬함수(sha3)를 통해서 할 수 있습니다.
+            // 솔리더티에는 문자열 비교에 대한 특별한 함수가 없습니다.
+            if(sha3(candidateList[i]) == sha3(cand)){
+                add = false; break;
+            }
+        }
+
+        if(add) {
+            candidateList[numberOfCandidates] = cand;
+            numberOfCandidates++;
+        }
+    }
+
+    // 투표를 하는 함수입니다.
+    function vote(string cand) {
+        // 하나의 계정은 한번의 투표만 결과에 반영됩니다.
+        if(voters[msg.sender]) { }
+        else{
+            voters[msg.sender] = true;
+            candidates[cand]++;
+        }
+    }
+
+    // 이미 투표했는지 확인합니다.
+    function alreadyVoted() constant returns(bool) {
+        if(voters[msg.sender])
+            return true;
+        else
+            return false;
+    }
+
+    //후보자의 수를 리턴합니다.
+    function getNumOfCandidates() constant returns(uint8) {
+        return numberOfCandidates;
+    }
+
+    //번호에 해당하는 후보의 이름을 리턴합니다.
+    function getCandidateString(uint8 number) constant returns(string) {
+        return candidateList[number];
+    }
+
+    //후보의 득표수를 리턴합니다.
+    function getScore(string cand) constant returns(uint) {
+        return candidates[cand];
+    }
+
+    //컨트랙트를 삭제합니다.
+    function killContract() {
+        if(contractOwner == msg.sender)
+            selfdestruct(contractOwner);
+    }
+}
